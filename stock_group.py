7a2ac22f81fd5fce48a6c51e9e9ccedfbf17a763
Johnny <40299325+maxmetis@users.noreply.github.com>
2020-08-21 14:16:35 +0800

    Add files via upload

--- stock_group.py
+++ stock_group.py
@@ -10,16 +10,35 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import time
+from selenium import webdriver
 
+url_main = 'https://www.cmoney.tw/follow/channel/stock-TWA00?chart=d&type=Personal'
 url = 'https://tw.stock.yahoo.com/q/q'
 
 
 headers = {
         'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4122.7 Mobile Safari/537.36'}
 
+#--------------------------------------
 
-stock_ids = ['1707', '2105','4906', '0050', '2330']
-#stock_ids = ['6269', '3105', '4906', '0050', '2330']
+driver = webdriver.Chrome(executable_path='./chromedriver')
+driver.implicitly_wait(20)
+driver.get(url=url_main)
+html_source = driver.page_source
+driver.quit()
+
+soup_main = BeautifulSoup(html_source, 'lxml')
+
+lis = soup_main.find_all('ul', class_='list7')[0].text.split('\n')
+
+tw_index = lis[1].replace('股價','')
+tw_change = lis[2].replace('漲跌','') 
+tw_ptg = lis[3].replace('漲幅','') 
+
+#print(tw_index, tw_change, tw_ptg)
+
+#-------------------------------------
+stock_ids = ['6269', '3105', '4906', '2105', '2330']
 
 df_index = ['股票代號', '成交', '漲跌','張數', '昨收', '開盤', '最高', '最低']
 
@@ -51,9 +70,9 @@ df['stock_name'] = stock_name
 df['stock_price'] = stock_price
 df['stock_change'] = stock_change
 
-print(df)
+#print(df)
 
-#LINE NOTIFY
+#LINE NOTIFY-------------------------------------
 
 def lineNotifyMessage(token, msg):
    headers = {
@@ -64,12 +83,20 @@ def lineNotifyMessage(token, msg):
    r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
    return r.status_code
 	
-message = '\n' + df.iloc[0,0] + '：' + df.iloc[0,1] + ' ; ' + df.iloc[0,2] \
+
+message_twse = '\n' + '加權指數：' + tw_index \
+             + '\n' + '漲跌：' + tw_change + ' (' + tw_ptg + ')'    
+
+
+#message_buying = 
+
+message_stock = '\n' + df.iloc[0,0] + '：' + df.iloc[0,1] + ' ; ' + df.iloc[0,2] \
         + '\n' + df.iloc[1,0] + '：' + df.iloc[1,1] + ' ; ' + df.iloc[1,2] \
         + '\n' + df.iloc[2,0] + '：' + df.iloc[2,1] + ' ; ' + df.iloc[2,2] \
         + '\n' + df.iloc[3,0] + '：' + df.iloc[3,1] + ' ; ' + df.iloc[3,2] \
         + '\n' + df.iloc[4,0] + '：' + df.iloc[4,1] + ' ; ' + df.iloc[4,2]   
             
-token = 'yfOAGBNnJXNoT57I9MM2aY9YgRKv9z1faDU1bbS2Ror'
+token = 'jkPdkZU4HqJ6z0UPzyLsVc5pNdnJu0RxqYLDy1iQ3or'
 
-lineNotifyMessage(token, message)
+lineNotifyMessage(token, message_twse)
+lineNotifyMessage(token, message_stock)
